9c805371be35277d269347dd3a14ad924a69f6c8
hbprotoss <gamespy1991@gmail.com>
2017-09-05 11:39:08 +0800

    runnable consumer

---
+++ service-consumer/src/main/java/io/hbprotoss/demo/HelloRemote.java
@@ -0,0 +1,12 @@
+package io.hbprotoss.demo;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name= "spring-cloud-producer")
+@RequestMapping(value = "/service-provider")
+public interface HelloRemote {
+    @RequestMapping(value = "/hello")
+    String hello(@RequestParam(value = "name") String name);
+}
