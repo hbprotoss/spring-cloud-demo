5b8eb5b3e2e8fb217f067c1376a8573d1b11e523
hbprotoss <gamespy1991@gmail.com>
2017-09-05 14:37:14 +0800

    consumer to tomcat

--- service-consumer/src/main/java/io/hbprotoss/demo/ConsumerApplication.java
+++ service-consumer/src/main/java/io/hbprotoss/demo/ConsumerApplication.java
@@ -2,13 +2,20 @@ package io.hbprotoss.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
-public class ConsumerApplication {
+public class ConsumerApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ConsumerApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
