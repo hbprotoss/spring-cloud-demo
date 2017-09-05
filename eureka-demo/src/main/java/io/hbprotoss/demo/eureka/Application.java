18c81e3620a913488fcec4449af8971b957e0030
hbprotoss <gamespy1991@gmail.com>
2017-09-05 10:39:07 +0800

    runnable

--- eureka-demo/src/main/java/io/hbprotoss/demo/eureka/Application.java
+++ eureka-demo/src/main/java/io/hbprotoss/demo/eureka/Application.java
@@ -3,7 +3,7 @@ package io.hbprotoss.demo.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
