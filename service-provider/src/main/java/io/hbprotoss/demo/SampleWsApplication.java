dd08823c7c8ec46daebf1fef2305c5470b15bb18
hbprotoss <gamespy1991@gmail.com>
2017-09-05 09:58:27 +0800

    1.5.6

--- service-provider/src/main/java/io/hbprotoss/demo/SampleWsApplication.java
+++ service-provider/src/main/java/io/hbprotoss/demo/SampleWsApplication.java
@@ -1,11 +1,13 @@
 package io.hbprotoss.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class SampleWsApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleWsApplication.class, args);
