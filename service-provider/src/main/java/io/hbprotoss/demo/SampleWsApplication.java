72694e88c63dc76bd2f6ecb0f7e0e6fbdcc94ac7
hbprotoss <gamespy1991@gmail.com>
2017-09-05 10:49:53 +0800

    service-provider to tomcat

--- service-provider/src/main/java/io/hbprotoss/demo/SampleWsApplication.java
+++ service-provider/src/main/java/io/hbprotoss/demo/SampleWsApplication.java
@@ -3,12 +3,20 @@ package io.hbprotoss.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class SampleWsApplication {
+public class SampleWsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SampleWsApplication.class);
+    }
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SampleWsApplication.class, args);
     }
