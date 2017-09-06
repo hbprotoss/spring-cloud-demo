54ce698366eabfc6f529241933a5f8a2ae480d42
hbprotoss <gamespy1991@gmail.com>
2017-09-06 10:44:12 +0800

    integrate soap

--- service-provider/src/main/java/io/hbprotoss/demo/WebServiceConfig.java
+++ service-provider/src/main/java/io/hbprotoss/demo/WebServiceConfig.java
@@ -1,24 +1,24 @@
-//package io.hbprotoss.demo;
-//
-//import io.hbprotoss.demo.service.HelloServiceImpl;
-//import org.apache.cxf.Bus;
-//import org.apache.cxf.jaxws.EndpointImpl;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import javax.xml.ws.Endpoint;
-//
-//@Configuration
-//public class WebServiceConfig {
-//
-//    @Autowired
-//    private Bus bus;
-//
-//    @Bean
-//    public Endpoint endpoint() {
-//        EndpointImpl endpoint = new EndpointImpl(bus, new HelloServiceImpl());
-//        endpoint.publish("/Hello");
-//        return endpoint;
-//    }
-//}
+package io.hbprotoss.demo;
+
+import io.hbprotoss.demo.service.HelloServiceImpl;
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.xml.ws.Endpoint;
+
+@Configuration
+public class WebServiceConfig {
+
+    @Autowired
+    private Bus bus;
+
+    @Bean
+    public Endpoint endpoint() {
+        EndpointImpl endpoint = new EndpointImpl(bus, new HelloServiceImpl());
+        endpoint.publish("/Hello");
+        return endpoint;
+    }
+}
--- service-provider/src/main/java/io/hbprotoss/demo/service/HelloService.java
+++ service-provider/src/main/java/io/hbprotoss/demo/service/HelloService.java
@@ -1,5 +1,8 @@
 package io.hbprotoss.demo.service;
 
+import javax.jws.WebService;
+
+@WebService
 public interface HelloService {
     String echo(String src);
 }
