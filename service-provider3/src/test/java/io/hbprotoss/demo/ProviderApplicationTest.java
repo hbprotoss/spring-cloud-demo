9a81b3ac05534f3db2c349d80e32cfa5f7252f13
hbprotoss <gamespy1991@gmail.com>
2017-09-14 15:55:13 +0800

    testTwoSpan()

--- service-provider3/src/test/java/io/hbprotoss/demo/ProviderApplicationTest.java
+++ service-provider3/src/test/java/io/hbprotoss/demo/ProviderApplicationTest.java
@@ -18,12 +18,26 @@ public class ProviderApplicationTest {
     private Tracer tracer;
 
     @Test
-    public void testTracer() throws InterruptedException {
-        System.out.println(tracer);
+    public void testOneSpan() throws InterruptedException {
         tracer.addTag("lalala", "123");
         Span span = tracer.createSpan("http:span");
         span.logEvent("Before Calling");
         Thread.sleep(1000L);
+        span.logEvent("After Calling");
+        tracer.close(span);
+    }
+
+    @Test
+    public void testTwoSpan() throws InterruptedException {
+        tracer.addTag("lalala", "123");
+        Span span = tracer.createSpan("http:span");
+        span.logEvent("Before Calling");
+        Thread.sleep(1000L);
+        Span span2 = tracer.createSpan("http:span2", span);
+        span2.logEvent("Before span2 Calling");
+        Thread.sleep(1000L);
+        span2.logEvent("After span2 Calling");
+        tracer.close(span2);
         span.logEvent("After Calling");
         tracer.close(span);
     }
