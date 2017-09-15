c73bc0b224f94b6842d93e6bc896f36ed983412f
hbprotoss <gamespy1991@gmail.com>
2017-09-15 10:19:03 +0800

    hot reload config

--- service-provider/src/main/java/io/hbprotoss/demo/ProviderApplication.java
+++ service-provider/src/main/java/io/hbprotoss/demo/ProviderApplication.java
@@ -1,6 +1,7 @@
 package io.hbprotoss.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
@@ -8,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @EnableEurekaClient
 @EnableFeignClients
 public class ProviderApplication extends SpringBootServletInitializer {
